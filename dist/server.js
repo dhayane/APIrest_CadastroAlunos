c529c418ac0f763268b97bf529cd287f989f004c
Dhayane Rocha <rochadhayane90@gmail.com>
2025-07-30 19:19:47 -0300

    Atualiza código para definir porta diretamente

--- dist/server.js
+++ dist/server.js
@@ -1,6 +1,6 @@
 "use strict"; function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _app = require('./app'); var _app2 = _interopRequireDefault(_app);
 
-const port = process.env.PORT || 3001;
+const port = 3001;
 _app2.default.listen(port, '0.0.0.0', () => {
   console.log(`Servidor rodando na porta ${port}`);
 });
--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import app from './app';
 
-const port = process.env.PORT || 3001;
+const port = 3001;
 app.listen(port, '0.0.0.0', () => {
   console.log(`Servidor rodando na porta ${port}`);
 });
