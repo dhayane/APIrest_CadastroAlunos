a15ed12a310c3182b051b9bcafc52f2755cb0004
Dhayane Rocha <rochadhayane90@gmail.com>
2025-07-30 20:35:00 -0300

    Adiciona 'public' ao package.json

---
+++ public/middlewares/cspMiddleware.js
@@ -0,0 +1,16 @@
+"use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _helmet = require('helmet'); var _helmet2 = _interopRequireDefault(_helmet);
+
+const cspMiddleware = (req, res, next) => {
+  _helmet2.default.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"], // Só permite fontes do mesmo domínio
+      styleSrc: ["'self'", 'https://fonts.googleapis.com', 'https://www.gstatic.com'], // Permite fontes e CSS do Google
+      styleSrcElem: ["'self'", 'https://fonts.googleapis.com', 'https://www.gstatic.com'],
+      fontSrc: ["'self'", 'https://fonts.gstatic.com'], // Permite fontes do Google
+      scriptSrc: ["'self'", 'https://www.gstatic.com'], // Permite scripts do Google
+      // Outras regras conforme necessário
+    }
+  })(req, res, next);
+};
+
+exports. default = cspMiddleware;
---
+++ public/models/Aluno.js
@@ -0,0 +1,75 @@
+"use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _sequelize = require('sequelize'); var _sequelize2 = _interopRequireDefault(_sequelize);
+
+ class Aluno extends _sequelize.Model {
+  static init(sequelize) {
+    super.init({
+      nome: {
+        type: _sequelize2.default.STRING,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [3, 50],
+            msg: 'Nome precisa ter entre 3 e 50 caracteres!',
+          },
+        },
+      },
+      sobrenome: {
+        type: _sequelize2.default.STRING,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [3, 50],
+            msg: 'Sobrenome precisa ter entre 3 e 50 caracteres!',
+          },
+        },
+      },
+      email: {
+        type: _sequelize2.default.STRING,
+        defaultValue: '',
+        unique: {
+          msg: 'E-mail já cadastrado.',
+        },
+        validate: {
+          isEmail: {
+            msg: 'E-mail inválido',
+          },
+        },
+      },
+      idade: {
+        type: _sequelize2.default.INTEGER,
+        defaultValue: '',
+        validate: {
+          isInt: {
+            msg: 'Idade precisa ser um número inteiro.',
+          },
+        },
+      },
+      peso: {
+        type: _sequelize2.default.FLOAT,
+        defaultValue: '',
+        validate: {
+          isFloat: {
+            msg: 'Peso precisa de um número inteiro ou de ponto flutuante',
+          },
+        },
+      },
+      altura: {
+        type: _sequelize2.default.FLOAT,
+        defaultValue: '',
+        validate: {
+          isFloat: {
+            msg: 'Altura precisa de um número inteiro ou de ponto flutuante',
+          },
+        },
+      },
+    }, {
+      sequelize,
+    });
+    return this;
+  }
+
+  static associate(models) {
+    this.hasMany(models.Photo, { foreignKey: 'aluno_id', as: 'Photo' });
+  }
+} exports.default = Aluno;
+
