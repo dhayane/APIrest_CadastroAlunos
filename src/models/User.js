a03b44be463c191b2720a4692fc0bf5f171c625f
Dhayane Rocha <rochadhayane90@gmail.com>
2025-07-30 14:15:57 -0300

    Adicionando arquivos

---
+++ src/models/User.js
@@ -0,0 +1,62 @@
+import Sequelize, { Model } from 'sequelize';
+import bcryptjs from 'bcryptjs';
+
+export default class User extends Model {
+  static init(sequelize) {
+    super.init({
+      nome: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [3, 50],
+            msg: 'Campo nome deve ter entre 3 e 16 caracteres.',
+          },
+        },
+      },
+      email: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        unique: {
+          msg: 'Email já existe',
+        },
+        validate: {
+          isEmail: {
+            msg: 'E-mail inválido.',
+          },
+        },
+      },
+      password_hash: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+      },
+      password: {
+        type: Sequelize.VIRTUAL,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [5, 20],
+            msg: 'A senha deve ter entre 5 e 20 caracteres.',
+          },
+        },
+      },
+    }, {
+      sequelize,
+    });
+
+    // Hook definido corretamente aqui
+    this.addHook('beforeSave', async (user) => {
+      if (user.password) {
+        user.password_hash = await bcryptjs.hash(user.password, 8);
+      }
+    });
+
+    return this;
+  }
+
+  passwordIsValid(password) {
+    return bcryptjs.compare(password, this.password_hash);
+  }
+}
+
+
